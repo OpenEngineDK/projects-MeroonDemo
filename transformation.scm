51142a3e150bbea38f21a749079f610b06c9b4bc
salomonhippie <salomonhippie@gmail.com>
2011-03-23 10:21:48 +0000

    bugfix in transformation matrix calculations. Experimenting with scmobj -> c conversion macros.

--- transformation.scm
+++ transformation.scm
@@ -38,41 +38,102 @@ c-declare-end
   (call-next-method))
 
 (define c-update-transformation-pivot!
-  (c-lambda (float float float float float float FloatArray) void
+  (c-lambda (scheme-object scheme-object scheme-object FloatArray FloatArray) void
 #<<UPDATE_TRANSFORMATION_PIVOT_END
 // printf("pivot\n");
-const float x      = ___arg1;
-const float y      = ___arg2;
-const float z      = ___arg3;
-const float px     = ___arg4;
-const float py     = ___arg5;
-const float pz     = ___arg6;
 
-const float dx = x - px;
-const float dy = y - py;
-const float dz = z - pz;
+const ___SCMOBJ scm_x  = ___VECTORREF(___arg1, 0);
+const ___SCMOBJ scm_y  = ___VECTORREF(___arg1, 4);
+const ___SCMOBJ scm_z  = ___VECTORREF(___arg1, 8);
+const ___SCMOBJ scm_px = ___VECTORREF(___arg2, 0);
+const ___SCMOBJ scm_py = ___VECTORREF(___arg2, 4);
+const ___SCMOBJ scm_pz = ___VECTORREF(___arg2, 8);
+const ___SCMOBJ scm_sx = ___VECTORREF(___arg3, 0);
+const ___SCMOBJ scm_sy = ___VECTORREF(___arg3, 4);
+const ___SCMOBJ scm_sz = ___VECTORREF(___arg3, 8);
 
-float* m     = ___arg7;
+float x;
+float y;
+float z;
+float px;
+float py;
+float pz;
+float sx;
+float sy;
+float sz;
+
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_x, x, 9);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_y, y, 9);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_z, z, 9);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_px, px, 9);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_py, py, 9);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_pz, pz, 9);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_sx, sx, 9);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_sy, sy, 9);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_sz, sz, 9);
+
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_sz, sz, 9);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_sy, sy, 9);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_sx, sx, 9);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_pz, pz, 9);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_py, py, 9);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_px, px, 9);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_z, z, 9);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_y, y, 9);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_x, x, 9);
+
+float dx = x - px;
+float dy = y - py;
+float dz = z - pz;
+
+const float* m_rot = ___arg4;
+float* m           = ___arg5;
 
 // fourth column
-m[12] = (m[0] - 1.0) * dx + m[4] * dy + m[8] * dz + x;
-m[13] = (m[1]) * dx + (m[5] - 1.0) * dy + m[9] * dz + y;
-m[14] = (m[2]) * dx + m[6] * dy + (m[10] - 1.0) * dz + z;
+m[12] = ((m_rot[0] - 1.0) * dx + m_rot[3] * dy + m_rot[6] * dz + x)   * sx;
+m[13] = ((m_rot[1]) * dx + (m_rot[4] - 1.0) * dy + m_rot[7] * dz + y) * sy;
+m[14] = ((m_rot[2]) * dx + m_rot[5] * dy + (m_rot[8] - 1.0) * dz + z) * sz;
 UPDATE_TRANSFORMATION_PIVOT_END
 ))
 
 (define c-update-transformation-pos!
-  (c-lambda (float float float FloatArray) void
+  (c-lambda (scheme-object scheme-object FloatArray) void
 #<<UPDATE_TRANSFORMATION_POS_END
-const float x  = ___arg1;
-const float y  = ___arg2;
-const float z  = ___arg3;
-float* m = ___arg4;
+
+const ___SCMOBJ scm_x  = ___VECTORREF(___arg1, 0);
+const ___SCMOBJ scm_y  = ___VECTORREF(___arg1, 4);
+const ___SCMOBJ scm_z  = ___VECTORREF(___arg1, 8);
+const ___SCMOBJ scm_sx = ___VECTORREF(___arg2, 0);
+const ___SCMOBJ scm_sy = ___VECTORREF(___arg2, 4);
+const ___SCMOBJ scm_sz = ___VECTORREF(___arg2, 8);
+
+float x;
+float y;
+float z;
+float sx;
+float sy;
+float sz;
+float* m = ___arg3;
+
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_x, x, 6);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_y, y, 6);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_z, z, 6);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_sx, sx, 6);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_sy, sy, 6);
+___BEGIN_CFUN_SCMOBJ_TO_FLOAT(scm_sz, sz, 6);
+
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_sz, sz, 6);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_sy, sy, 6);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_sx, sx, 6);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_z, z, 6);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_y, y, 6);
+___END_CFUN_SCMOBJ_TO_FLOAT(scm_x, x, 6);
+
 
 // fourth column
-m[12] = -x;
-m[13] = -y;
-m[14] = -z;
+m[12] = -x * sx;
+m[13] = -y * sy;
+m[14] = -z * sz;
 UPDATE_TRANSFORMATION_POS_END
 ))
 
@@ -87,36 +148,34 @@ float* m           = ___arg5;
 
 // first column
 m[0] = m_rot[0] * x;
-m[1] = m_rot[1];
-m[2] = m_rot[2];
+m[1] = m_rot[1] * y;
+m[2] = m_rot[2] * z;
 
 // second column
-m[4] = m_rot[3];
+m[4] = m_rot[3] * x;
 m[5] = m_rot[4] * y;
-m[6] = m_rot[5];
+m[6] = m_rot[5] * z;
 
 // third column
-m[8]  = m_rot[6];
-m[9]  = m_rot[7];
+m[8]  = m_rot[6] * x;
+m[9]  = m_rot[7] * y;
 m[10] = m_rot[8] * z;
 UPDATE_TRANSFORMATION_ROT_AND_SCL_END
 ))
 
 (define (update-transformation-pos! o)
-  (with-access o (Transformation translation pivot c-matrix)
+  (with-access o (Transformation translation rotation scaling pivot c-matrix)
     (if pivot
         (c-update-transformation-pivot!
-         (vector-ref translation 0)
-         (vector-ref translation 1)
-         (vector-ref translation 2)
-         (vector-ref pivot 0)
-         (vector-ref pivot 1)
-         (vector-ref pivot 2)
-         c-matrix)
+         translation
+         pivot
+         scaling
+         (Quaternion-c-matrix rotation)
+         c-matrix
+         )
         (c-update-transformation-pos!
-         (vector-ref translation 0)
-         (vector-ref translation 1)
-         (vector-ref translation 2)
+         translation
+         scaling
          c-matrix))))
 
 
@@ -130,12 +189,10 @@ UPDATE_TRANSFORMATION_ROT_AND_SCL_END
      c-matrix)
     (if pivot
         (c-update-transformation-pivot!
-         (vector-ref translation 0)
-         (vector-ref translation 1)
-         (vector-ref translation 2)
-         (vector-ref pivot 0)
-         (vector-ref pivot 1)
-         (vector-ref pivot 2)
+         translation
+         pivot
+         scaling
+         (Quaternion-c-matrix rotation)
          c-matrix))))
 
 ;;; methods
