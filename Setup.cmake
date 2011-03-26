09f6144b6eaf92a7e3f6412677b92cc7c3cadf03
zerny <zerny@openengine.dk>
2011-03-26 15:54:59 +0000

    Some CMake/Gambit hacking.

--- Setup.cmake
+++ Setup.cmake
@@ -1,3 +1,4 @@
+# Some gambit variables...
 SET(GAMBITC ${GAMBIT_DEPS_BIN_DIR}/gsc)
 LINK_DIRECTORIES(${GAMBIT_DEPS_LIB_DIR})
 INCLUDE_DIRECTORIES(${GAMBIT_DEPS_INCLUDE_DIR})
@@ -8,23 +9,25 @@ IF(CMAKE_BUILD_TYPE STREQUAL debug)
   SET(GAMBITC_FLAGS ${GAMBITC_FLAGS} -debug -track-scheme)
 ENDIF(CMAKE_BUILD_TYPE STREQUAL debug)
 
-# Setup the Meroon Object System
+# Create a Meroon library that extends plain gambit
+# Target: GAMBIT_MEROON
 SET(GAMBIT_MEROON_SRC  ${MEROON_DEPS_LIB_DIR}/_meroon.c)
-SET(GAMBIT_MEROON_LINK ${MEROON_DEPS_LIB_DIR}/_meroon_link)
+SET(GAMBIT_MEROON_LINK_FILE ${MEROON_DEPS_LIB_DIR}/_meroon_link)
 SET_SOURCE_FILES_PROPERTIES(
-  ${GAMBIT_MEROON_SRC} ${GAMBIT_MEROON_LINK}
-  PROPERTIES LANGUAGE ${GAMBIT_OUTPUT_LANGUAGE} COMPILE_FLAGS "-w -D___LIBRARY")
-ADD_LIBRARY(Meroon ${GAMBIT_MEROON_SRC} ${GAMBIT_MEROON_LINK})
-TARGET_LINK_LIBRARIES(Meroon ${GAMBIT_LIBRARIES})
+  ${GAMBIT_MEROON_SRC} ${GAMBIT_MEROON_LINK_FILE}
+  PROPERTIES
+  LANGUAGE ${GAMBIT_OUTPUT_LANGUAGE}
+  COMPILE_FLAGS "-w -D___LIBRARY")
+ADD_LIBRARY(GAMBIT_MEROON ${GAMBIT_MEROON_SRC} ${GAMBIT_MEROON_LINK_FILE})
+TARGET_LINK_LIBRARIES(GAMBIT_MEROON ${GAMBIT_LIBRARIES})
 
-# (GAMBIT_ADD_EXECUTABLE target files ...) 
-# Will define
-# - target           : cmake target for the executable
+# This is an interal helper function for GAMBIT_ADD_LIBRARY/EXECUTABLE
+# Generates:
 # - target_C_FILES   : generated c files
 # - target_LINK_FILE : generated link file
 # - target_SCM_FILES : source scheme files (absolute path)
 # - target_TMP_FILES : temporary output files (absolute path)
-MACRO(GAMBIT_ADD_EXECUTABLE target)
+MACRO(GAMBIT_ADD_TARGET_HELPER type target extends)
   FOREACH(SRC ${ARGN})
     SET(SRC_SCM "${CMAKE_CURRENT_SOURCE_DIR}/${SRC}")
     STRING(REPLACE ".scm" ".c" SRC_TMP ${SRC_SCM})
@@ -33,22 +36,36 @@ MACRO(GAMBIT_ADD_EXECUTABLE target)
     SET(${target}_TMP_FILES ${${target}_TMP_FILES} ${SRC_TMP})
     SET(${target}_C_FILES   ${${target}_C_FILES}   ${SRC_C})
   ENDFOREACH(SRC)
-  SET(${target}_LINK_FILE ${target}_link.c)
+  SET(${target}_LINK_FILE ${target}_link)
   SET_SOURCE_FILES_PROPERTIES(
-    ${${target}_C_FILES} ${${target}_LINK_FILE}
+    ${${target}_C_FILES} ${${target}_LINK_FILE}.c
     PROPERTIES 
     GENERATED 1
     LANGUAGE ${GAMBIT_OUTPUT_LANGUAGE}
-    COMPILE_FLAGS "-w"
+    COMPILE_FLAGS "-w -D___${type}"
     )
   ADD_CUSTOM_COMMAND(
-    OUTPUT ${${target}_C_FILES} ${${target}_LINK_FILE}
-    COMMAND ${GAMBITC} ${GAMBITC_FLAGS} -link -l ${GAMBIT_MEROON_LINK} -o ${${target}_LINK_FILE} ${${target}_SCM_FILES}
+    OUTPUT ${${target}_C_FILES} ${${target}_LINK_FILE}.c
+    COMMAND echo ${GAMBITC} ${GAMBITC_FLAGS} -link -l ${${extends}_LINK_FILE} -o ${${target}_LINK_FILE}.c ${${target}_SCM_FILES}
+    COMMAND ${GAMBITC} ${GAMBITC_FLAGS} -link -l ${${extends}_LINK_FILE} -o ${${target}_LINK_FILE}.c ${${target}_SCM_FILES}
     COMMAND mv ${${target}_TMP_FILES} .
     DEPENDS ${${target}_SCM_FILES} #${ARGN}
     COMMENT "Generating ${GAMBIT_OUTPUT_LANGUAGE} for target ${target}"
     )
-  ADD_EXECUTABLE(${target} ${${target}_C_FILES} ${${target}_LINK_FILE})
+ENDMACRO(GAMBIT_ADD_TARGET_HELPER)
+
+# (GAMBIT_ADD_LIBRARY target extends files ...)
+MACRO(GAMBIT_ADD_LIBRARY target extends)
+  GAMBIT_ADD_TARGET_HELPER(LIBRARY ${ARGV})
+  ADD_LIBRARY(${target} ${${target}_C_FILES} ${${target}_LINK_FILE}.c)
   #TARGET_LINK_LIBRARIES(${target} gambc m dl util)
-  TARGET_LINK_LIBRARIES(${target} Meroon)
+  TARGET_LINK_LIBRARIES(${target} ${extends})
+ENDMACRO(GAMBIT_ADD_LIBRARY)
+
+# (GAMBIT_ADD_EXECUTABLE target extends files ...) 
+MACRO(GAMBIT_ADD_EXECUTABLE target extends)
+  GAMBIT_ADD_TARGET_HELPER(PRIMAL ${ARGV})
+  ADD_EXECUTABLE(${target} ${${target}_C_FILES} ${${target}_LINK_FILE}.c)
+  #TARGET_LINK_LIBRARIES(${target} gambc m dl util)
+  TARGET_LINK_LIBRARIES(${target} ${extends})
 ENDMACRO(GAMBIT_ADD_EXECUTABLE)
