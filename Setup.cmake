b7e0894a3b80260896e0b7d6c3e0ec91800bceca
zerny <zerny@openengine.dk>
2011-04-05 21:01:53 +0000

    Now with support for loadable scheme modules.

--- Setup.cmake
+++ Setup.cmake
@@ -34,9 +34,10 @@ MACRO(GAMBIT_ADD_TARGET_HELPER type target extends)
     STRING(REPLACE ".scm" ".c" SRC_C   ${SRC})
     SET(${target}_SCM_FILES ${${target}_SCM_FILES} ${SRC_SCM})
     SET(${target}_TMP_FILES ${${target}_TMP_FILES} ${SRC_TMP})
-    SET(${target}_C_FILES   ${${target}_C_FILES}   ${SRC_C})
+    SET(${target}_C_FILES   ${${target}_C_FILES}   ${type}/${SRC_C})
+    #SET(${target}_C_SHARED_FILES ${${target}_C_SHARED_FILES} "${type}/_${SRC_C}")
   ENDFOREACH(SRC)
-  SET(${target}_LINK_FILE ${target}_link)
+  SET(${target}_LINK_FILE ${type}/${target}_link)
   SET_SOURCE_FILES_PROPERTIES(
     ${${target}_C_FILES} ${${target}_LINK_FILE}.c
     PROPERTIES 
@@ -44,23 +45,51 @@ MACRO(GAMBIT_ADD_TARGET_HELPER type target extends)
     LANGUAGE ${GAMBIT_OUTPUT_LANGUAGE}
     COMPILE_FLAGS "-w -D___${type}"
     )
+  # SET_SOURCE_FILES_PROPERTIES(
+  #   ${${target}_C_SHARED_FILES} ${${target}_LINK_FILE}.c
+  #   PROPERTIES 
+  #   GENERATED 1
+  #   LANGUAGE ${GAMBIT_OUTPUT_LANGUAGE}
+  #   COMPILE_FLAGS "-w -D___DYNAMIC"
+  #   )
+
+  IF(${type} STREQUAL "DYNAMIC")
   ADD_CUSTOM_COMMAND(
-    OUTPUT ${${target}_C_FILES} ${${target}_LINK_FILE}.c
-    COMMAND echo ${GAMBITC} ${GAMBITC_FLAGS} -link -l ${${extends}_LINK_FILE} -o ${${target}_LINK_FILE}.c ${${target}_SCM_FILES}
+    OUTPUT ${${target}_C_FILES} ${${target}_LINK_FILE}.o1.c 
+         # ${${target}_C_SHARED_FILES} _${${target}_LINK_FILE}.c
+    COMMAND mkdir -p ${type}/
+    COMMAND ${GAMBITC} ${GAMBITC_FLAGS} -link -flat -o ${${target}_LINK_FILE}.o1.c ${${target}_SCM_FILES} >/dev/null
+    COMMAND echo mv ${${target}_TMP_FILES} ${type}/
+    COMMAND mv ${${target}_TMP_FILES} ${type}/
+    DEPENDS ${${target}_SCM_FILES}
+    COMMENT "Generating ${GAMBIT_OUTPUT_LANGUAGE} for target ${target}")
+  ELSE(${type} STREQUAL "DYNAMIC")
+  ADD_CUSTOM_COMMAND(
+    OUTPUT ${${target}_C_FILES} ${${target}_LINK_FILE}.c 
+         # ${${target}_C_SHARED_FILES} _${${target}_LINK_FILE}.c
+    COMMAND mkdir -p ${type}/
     COMMAND ${GAMBITC} ${GAMBITC_FLAGS} -link -l ${${extends}_LINK_FILE} -o ${${target}_LINK_FILE}.c ${${target}_SCM_FILES}
-    COMMAND mv ${${target}_TMP_FILES} .
+    COMMAND echo mv ${${target}_TMP_FILES} ${type}/
+    COMMAND mv ${${target}_TMP_FILES} ${type}/
     DEPENDS ${${target}_SCM_FILES} #${ARGN}
-    COMMENT "Generating ${GAMBIT_OUTPUT_LANGUAGE} for target ${target}"
-    )
+    COMMENT "Generating ${GAMBIT_OUTPUT_LANGUAGE} for target ${target}")
+  ENDIF(${type} STREQUAL "DYNAMIC")
 ENDMACRO(GAMBIT_ADD_TARGET_HELPER)
 
 # (GAMBIT_ADD_LIBRARY target extends files ...)
 MACRO(GAMBIT_ADD_LIBRARY target extends)
   GAMBIT_ADD_TARGET_HELPER(LIBRARY ${ARGV})
-  ADD_LIBRARY(${target} ${${target}_C_FILES} ${${target}_LINK_FILE}.c)
+  ADD_LIBRARY(${target} STATIC ${${target}_C_FILES} ${${target}_LINK_FILE}.c)
   #TARGET_LINK_LIBRARIES(${target} gambc m dl util)
   TARGET_LINK_LIBRARIES(${target} ${extends})
 ENDMACRO(GAMBIT_ADD_LIBRARY)
+
+MACRO(GAMBIT_ADD_SHARED_LIBRARY target extends)
+  GAMBIT_ADD_TARGET_HELPER(DYNAMIC ${ARGV})
+  ADD_LIBRARY(${target} SHARED ${${target}_C_FILES} ${${target}_LINK_FILE}.o1.c)
+  #TARGET_LINK_LIBRARIES(${target} gambc m dl util)
+  TARGET_LINK_LIBRARIES(${target} ${extends})
+ENDMACRO(GAMBIT_ADD_SHARED_LIBRARY)
 
 # (GAMBIT_ADD_EXECUTABLE target extends files ...) 
 MACRO(GAMBIT_ADD_EXECUTABLE target extends)
