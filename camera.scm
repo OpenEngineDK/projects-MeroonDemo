51acd7bed07d00a110df495395a44a110206f59f
salomonhippie <salomonhippie@gmail.com>
2011-04-11 16:51:32 +0000

    added unproject function in camera.scm.

--- camera.scm
+++ camera.scm
@@ -86,3 +86,94 @@ UPDATE_PROJECTION_END
 (define-method (translate! (cam Camera) x y z)
   (translate! (Camera-view cam) x y z))
 
+(c-declare #<<C-DECLARE-END
+#include <Math/Matrix.h>
+#include <Math/Vector.h>
+
+using OpenEngine::Math::Matrix;
+using OpenEngine::Math::Vector;
+
+Matrix<4,4,float> proj_m;
+Matrix<4,4,float> view_m;
+
+C-DECLARE-END
+)
+
+(c-define (set-vector v x y z)
+    (scheme-object float float float) void "set_vector_scm" ""
+  (vector-set! v 0 x)
+  (vector-set! v 1 y)
+  (vector-set! v 2 z))
+
+;; x and y in window space [0;1] and z is the target depth value [0;1] 
+(define (unproject cam x y z)
+  (with-access cam (Camera proj view)
+    ((c-lambda ((pointer "float")) void 
+#<<SET-PROJ-END
+proj_m = Matrix<4,4,float>(___arg1);
+proj_m.Transpose();
+SET-PROJ-END
+) 
+     (Projection-c-matrix proj))
+    (with-access view (Transformation translation rotation)
+      ((c-lambda (float float float float float float float) void
+#<<SET-VIEW-END
+view_m = Matrix<4,4,float>();
+
+const float pos_x  = ___arg1;
+const float pos_y  = ___arg2;
+const float pos_z  = ___arg3;
+
+const float w  = ___arg4;
+const float x  = ___arg5;
+const float y  = ___arg6;
+const float z  = ___arg7;
+
+// fourth column
+view_m(0,3) = -pos_x;
+view_m(1,3) = -pos_y;
+view_m(2,3) = -pos_z;
+
+// first column
+view_m(0,0) = 1-2*y*y-2*z*z;
+view_m(1,0) = 2*x*y-2*w*z;
+view_m(2,0) = 2*x*z+2*w*y;
+
+// second column
+view_m(0,1) = 2*x*y+2*w*z;
+view_m(1,1) = 1-2*x*x-2*z*z;
+view_m(2,1) = 2*y*z-2*w*x;
+
+// third column
+view_m(0,2) = 2*x*z-2*w*y;
+view_m(1,2) = 2*y*z+2*w*x;
+view_m(2,2) = 1-2*x*x-2*y*y;
+
+SET-VIEW-END
+)
+       (vector-ref translation 0)
+       (vector-ref translation 1)
+       (vector-ref translation 2)
+       (Quaternion-w rotation)
+       (Quaternion-x rotation)
+       (Quaternion-y rotation)
+       (Quaternion-z rotation))))
+  (let ([point (vector 0. 0. 0.)])
+    ((c-lambda (float float float scheme-object) void
+#<<CALC-PROJ-END
+const float x  = ___arg1;
+const float y  = ___arg2;
+const float z  = ___arg3;
+Matrix<4,4,float> inv = (proj_m * view_m).GetInverse();
+Vector<4,float> pos4(x * 2.0 - 1.0, y * 2.0 - 1.0, z, 1.0);
+pos4 = inv * pos4;
+pos4 = Vector<4,float>(pos4[0]/pos4[3], 
+                       pos4[1]/pos4[3], 
+                       pos4[2]/pos4[3],
+                       1.0);
+
+set_vector_scm(___arg4, pos4[0], pos4[1], pos4[2]);
+CALC-PROJ-END
+)
+     x y z point)
+    point))
